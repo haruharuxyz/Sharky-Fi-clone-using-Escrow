1ada404ac9fcb9618994d719f59b2d3129b9fe0f
haruharuxyz <137798783+haruharuxyz@users.noreply.github.com>
2023-11-26 00:26:39 +0700

    declare program address

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("7Y6WHQ71P2qZTRobEwEyxBEbXoL6a2e45Hf6ffpf1ZXS");
 
 #[program]
 pub mod nft_lend_borrow {
